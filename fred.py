54fe1dd4a8a079716fdc1eeba019385cc84f745c
CNDRD <20823071+CNDRD@users.noreply.github.com>
2020-08-07 23:59:13 +0200

    cog reload command needs permission now

--- fred.py
+++ fred.py
@@ -7,6 +7,7 @@ import os
 config = yaml.load(open("config.yml"), Loader=yaml.FullLoader)
 token = config.get('token')
 prefix = str(config.get('prefix'))
+good_guys = config.get('admin_user_ids')
 
 ################################## Functions ##################################
 def cogs(cc):
@@ -34,14 +35,17 @@ async def on_ready():
 
 @client.command(aliases=['rc'])
 async def reload_cogs(ctx):
-    async with ctx.typing():
-        y = 0
-        for filename in os.listdir('./cogs'):
-            if filename.endswith('.py'):
-                client.unload_extension(f'cogs.{filename[:-3]}')
-                client.load_extension(f'cogs.{filename[:-3]}')
-                y += 1
-        print(f"\nSuccesfully reloaded {y}/{cog_count} cogs!\n")
-    await ctx.message.add_reaction('✅')
+    if str(ctx.author.id) in good_guys:
+        async with ctx.typing():
+            y = 0
+            for filename in os.listdir('./cogs'):
+                if filename.endswith('.py'):
+                    client.unload_extension(f'cogs.{filename[:-3]}')
+                    client.load_extension(f'cogs.{filename[:-3]}')
+                    y += 1
+            print(f"\nSuccesfully reloaded {y}/{cog_count} cogs!\n")
+        await ctx.message.add_reaction('✅')
+    else:
+        await ctx.send("❗ You don't have permission to do that ❗")
 
 client.run(token)
